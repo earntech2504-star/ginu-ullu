6384f7dfef9762fe3aaae4be6927280e9ae58a0c
earntech2504-star <earntech2504@gmail.com>
2026-05-21 18:30:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,7 @@
-const CACHE = 'ginu-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE_NAME = 'ginu-v1';
 self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./','./index.html'])));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request).then(networkRes => {
-        if(networkRes && networkRes.status === 200 && e.request.method === 'GET'){
-          const copy = networkRes.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        }
-        return networkRes;
-      }).catch(()=> caches.match('./index.html'));
-    })
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
